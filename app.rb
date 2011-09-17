f6c6867cbf9912f6fab7a55e3cf6575fbf2cadf2
Kevin Old <kevin@kevinold.com>
2011-09-17 03:15:29 -0500

    add twitter bootstrap; start using haml templates

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require 'rubygems'
 require 'sinatra'
 require 'pp'
 require 'model'
+require 'haml'
+require 'sass'
+
+configure do
+  # Configure HAML and SASS
+  set :haml, { :format => :html5 }
+  set :sass, { :style => :compressed } if ENV['RACK_ENV'] == 'production'
+end
 
 get '/' do
   @tweets = Tweet.all(:order => [:created_at.desc])
-  erb :index
+  haml :index
 end
